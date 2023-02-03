039a54481ed16033204b9a74dbc100620fe93dab
Peter Yoachim <yoachim@uw.edu>
2023-02-03 15:32:37 -0800

    streaking

--- neo_check/ss_observations_3.sh
+++ neo_check/ss_observations_3.sh
@@ -1,7 +1,9 @@
 # ugh, hate how convoluted these calls are
 run_moving_calc --obs_file baseline_v3.0_10yrs__vatiras_granvik_10k_obs.txt --simulation_db baseline_v3.0_10yrs.db --orbit_file /Users/yoachim/rubin_sim_data/orbits/vatiras_granvik_10k.txt --out_dir baseline_v3.0_10yrs_ss --objtype Vatira --start_time 60218.0  ; run_moving_fractions --work_dir baseline_v3.0_10yrs_ss --metadata Vatira --start_time 60218.0
 
-run_moving_calc --obs_file baseline_v3.0_10yrs__vatiras_granvik_10k_obs_streaked.txt --simulation_db baseline_v3.0_10yrs.db --orbit_file /Users/yoachim/rubin_sim_data/orbits/vatiras_granvik_10k.txt --out_dir franken_streaked_v2.99_10yrs_ss --objtype Vatira --start_time 60218.0  ; run_moving_fractions --work_dir baseline_streaked_v3.0_10yrs_ss --metadata Vatira --start_time 60218.0
+run_moving_calc --obs_file baseline_v3.0_10yrs__vatiras_granvik_10k_obs_streaked.txt --simulation_db baseline_v3.0_10yrs.db --orbit_file /Users/yoachim/rubin_sim_data/orbits/vatiras_granvik_10k.txt --out_dir baseline_streaked_v3.0_10yrs_ss --objtype Vatira --start_time 60218.0  ; run_moving_fractions --work_dir baseline_streaked_v3.0_10yrs_ss --metadata Vatira --start_time 60218.0
+run_moving_calc --obs_file baseline_v3.0_10yrs__wide_vatiras_granvik_10k_obs_streaked.txt --simulation_db baseline_v3.0_10yrs.db --orbit_file /Users/yoachim/rubin_sim_data/orbits/vatiras_granvik_10k.txt --out_dir baseline_wide_v3.0_10yrs_ss --objtype Vatira --start_time 60218.0  ; run_moving_fractions --work_dir baseline_wide_v3.0_10yrs_ss --metadata Vatira --start_time 60218.0
 
-add_run franken_streaked_v2.99_10yrs_ss
 add_run baseline_v3.0_10yrs_ss
+add_run baseline_streaked_v3.0_10yrs_ss
+add_run baseline_wide_v3.0_10yrs_ss 
